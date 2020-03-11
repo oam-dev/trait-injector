70e06e197dd9bbc3771b48ea21d1f5bcd3f42ddf
Hongchao Deng <hongchaodeng1@gmail.com>
2020-03-10 23:00:23 -0700

    add scaffold code for StatefulsetTargetInjector

--- pkg/injector/statefulset_injector.go
+++ pkg/injector/statefulset_injector.go
@@ -1,1 +1,29 @@
 package injector
+
+import (
+	"github.com/go-logr/logr"
+	"github.com/oam-dev/trait-injector/pkg/plugin"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+)
+
+var _ plugin.TargetInjector = &StatefulsetTargetInjector{}
+
+type StatefulsetTargetInjector struct {
+	Log logr.Logger
+}
+
+func (ti *StatefulsetTargetInjector) Name() string {
+	return "StatefulsetTargetInjector"
+}
+
+func (ti *StatefulsetTargetInjector) Match(k metav1.GroupVersionKind) bool {
+	if k.Group == "apps" && k.Version == "v1" && k.Kind == "StatefulSet" {
+		return true
+	}
+	return false
+}
+
+func (ti *StatefulsetTargetInjector) Inject(ctx plugin.TargetContext, raw runtime.RawExtension) ([]webhook.JSONPatchOp, error) {
+}
