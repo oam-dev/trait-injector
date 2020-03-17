fdd4911f89abee764f35524b86ad7b2cdf3a24d2
hongchaodeng <hongchaodeng1@gmail.com>
2020-03-17 13:52:28 -0700

    fix e2e

--- e2e-test/suite_test.go
+++ e2e-test/suite_test.go
@@ -19,7 +19,6 @@ import (
 	"context"
 
 	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
 
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
--- e2e-test/webhook_test.go
+++ e2e-test/webhook_test.go
@@ -2,14 +2,12 @@ package controllers
 
 import (
 	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
 )
 
 var _ = Context("Inside of a new namespace", func() {
 
 	Describe("when creating Deployment", func() {
 		It("should inject env", func() {
-			Expect(1).NotTo(HaveOccurred())
 		})
 		It("should inject file", func() {
 		})
